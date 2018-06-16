6ff0ebee48549e77894797fe397349bbe08acd38
viswa kodela <viswakodela@viswas-mbp.local>
2018-06-16 12:21:02 -0400

    Login Segment and Register Segment Constraints made, SignOut if the currentUser == nil

--- ChatApplication/Controllers/ViewController.swift
+++ ChatApplication/Controllers/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UITableViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+//        ref.updateChildValues(["someValue": 123123])
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: .plain, target: self, action: #selector(handleLogOut))
+        
+        if Auth.auth().currentUser == nil {
+            handleLogOut()
+        }
         
     }
     
     @objc func handleLogOut() {
+        
+        do {
+            try Auth.auth().signOut()
+        }catch {
+            print("Logout Error")
+        }
+        
         
         let loginController = LoginViewController()
         
